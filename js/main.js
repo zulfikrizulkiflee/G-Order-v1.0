b66813232b0c2cd42a7b0dc9700cf7eb1215a355
Zulfikri Zulkiflee <zulfikrizulkiflee@yahoo.com.my>
2017-11-27 09:57:49 +0800

    cordova plugin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-//function onLoad() {
-//    document.addEventListener("deviceready", onDeviceReady, false);
-//    document.addEventListener("backbutton", onBackKeyDown, false);
-//}
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
 // device APIs are available
 $(".page").on("swiperight", function () {
     $('body').addClass("opensidebar");
@@ -10,7 +10,7 @@ $(".page").on("swipeleft", function () {
     $('body').removeClass("opensidebar");
 });
 //
-//function onDeviceReady() {
+function onDeviceReady() {
     var local_api = "api/";
     var remote_api = "http://www.zfikri.tk/obe_api/";
     var api = "";
@@ -949,26 +949,26 @@ $(".page").on("swipeleft", function () {
             , html: html
         });
     }
-//}
+}
 
-//var lastTimeBackPress=0;
-//var timePeriodToExit=2000;
-//
-//function onBackKeyDown(e){
-//    e.preventDefault();
-//    e.stopPropagation();
-//    if(new Date().getTime() - lastTimeBackPress < timePeriodToExit){
-//        navigator.app.exitApp();
-//    }else{
-//        window.plugins.toast.showWithOptions(
-//            {
-//                message: "Press again to exit.",
-//                duration: "short", // which is 2000 ms. "long" is 4000. Or specify the nr of ms yourself.
-//                position: "bottom",
-//                addPixelsY: -40  // added a negative value to move it up a bit (default 0)
-//            }
-//        );
-//
-//        lastTimeBackPress=new Date().getTime();
-//    }
-//}
+var lastTimeBackPress=0;
+var timePeriodToExit=2000;
+
+function onBackKeyDown(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if(new Date().getTime() - lastTimeBackPress < timePeriodToExit){
+        navigator.app.exitApp();
+    }else{
+        window.plugins.toast.showWithOptions(
+            {
+                message: "Press again to exit.",
+                duration: "short", // which is 2000 ms. "long" is 4000. Or specify the nr of ms yourself.
+                position: "bottom",
+                addPixelsY: -40  // added a negative value to move it up a bit (default 0)
+            }
+        );
+
+        lastTimeBackPress=new Date().getTime();
+    }
+}
